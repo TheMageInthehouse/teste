3fc9e53468371b397f2068ee3355518fded8d408
BRUNO MATEUS FELIX VICENTIN <vicentin.7910@aluno.pr.senac.br>
2023-09-11 11:31:32 -0300

    alterado

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -14,6 +14,8 @@ public class Repository {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("alterado");
+        
         // TODO code application logic here
     }
     
